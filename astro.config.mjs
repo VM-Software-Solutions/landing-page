1c14363bb66046bde99cee9778d9832e0d494696
Martín Jesús Crosetto <martincrosetto312@gmail.com>
2026-01-18 17:18:57 -0300

    fix: stop it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-  import vercel from '@astrojs/vercel'; // 👈 Importás el adapter
 
 
 export default defineConfig({
   integrations: [tailwind()],
   output: "static",                     // 👈 Le decís a Astro que genere un sitio estático
-  adapter: vercel(),                     // 👈 Le decís a Astro que use Vercel
 });
